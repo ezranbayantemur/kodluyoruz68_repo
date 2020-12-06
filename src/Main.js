d2cef85fb5122f0556d4a751d44bbdb747bcb6f9
ezranbayantemur <ezranbayantemur@gmail.com>
2020-12-06 10:29:29 +0300

    Created our custom component with multiple props

--- src/Main.js
+++ src/Main.js
@@ -1,38 +1,34 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
-import CityItem from './components/CityItem';
-
-const cityList = [
-  'Manisa',
-  'Çanakkale',
-  'Ankara',
-  'Eskişehir',
-  'Konya',
-  'Antalya',
-  'Bursa',
-];
-
-function renderCity() {
-  return cityList.map((city) => {
-    return <CityItem cityName={city} />;
-  });
-}
+import {SafeAreaView, View, Text, Button} from 'react-native';
+import MyButton from './component/MyButton';
 
 function Main() {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <View style={styles.container}>{renderCity()}</View>
+      <View style={{flex: 1}}>
+        <Text style={{fontSize: 50}}>Hello!</Text>
+
+        <MyButton
+          myTitle="Ezran"
+          myPress={() => alert('Selam Ezran')}
+          onSend={(val) => alert('Value is: ' + val)}
+        />
+        <MyButton
+          myTitle="Ayşe"
+          myPress={() => alert('İzmir')}
+          onSend={(val) => alert('Value is: ' + val)}
+        />
+
+        <MyButton
+          myTitle="Ali"
+          myPress={() => alert('Ankara!')}
+          onSend={(val) => alert('Value is: ' + val)}
+        />
+      </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#bbdefb',
-    justifyContent: 'space-between',
-  },
-});
 
 export default Main;
---
+++ src/component/MyButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+const MyButton = (props) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.buttonContainer} onPress={props.myPress}>
+        <Text>{props.myTitle}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.buttonContainer, {backgroundColor: 'red'}]}
+        onPress={() => props.onSend(5)}>
+        <Text>Send</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default MyButton;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#eceff1',
+    padding: 10,
+    margin: 10,
+  },
+  buttonContainer: {
+    backgroundColor: 'cyan',
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+  },
+});
