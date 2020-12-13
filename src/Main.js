5eaa6bdc7606c8e8edd7c179396ce8986607e7d4
ezranbayantemur <ezranbayantemur@gmail.com>
2020-12-13 10:04:44 +0300

    New example project

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+function Main() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+export default Main;
