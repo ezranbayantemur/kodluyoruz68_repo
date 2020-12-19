c4edd271030f27b5769051499cc522633b31fc96
ezranbayantemur <ezranbayantemur@gmail.com>
2020-12-19 13:28:35 +0300

    Passing parameters with state

--- src/pages/FriendsPage.js
+++ src/pages/FriendsPage.js
@@ -2,12 +2,11 @@ import React from 'react';
 import {View, Text, Button} from 'react-native';
 
 function FriendScreen({navigation, route}) {
-  console.log(route.params);
-  const {userName} = route.params;
-  console.log(userName);
+  console.log(route);
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Friends Screen</Text>
+      <Text>User: {route.params.userText}</Text>
       <Button title="Go Back" onPress={() => navigation.goBack()} />
     </View>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native';
 
 function HomeScreen(props) {
+  const [text, setText] = useState('');
+
   function goTo() {
-    props.navigation.navigate('Friends', {userId: 100, userName: 'Selamlar!!'});
+    props.navigation.navigate('Friends', {userText: text});
   }
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button title="Go to friends" onPress={goTo} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+
+        <View style={styles.inputContainer}>
+          <TextInput onChangeText={(val) => setText(val)} />
+        </View>
+
+        <Button title="Go to friends" onPress={goTo} />
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    backgroundColor: '#e0e0e0',
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+});
 
 export {HomeScreen};
