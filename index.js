6034a21b7bb6b01c1c10ec5f3f39150dc70a1152
ezranbayantemur <ezranbayantemur@gmail.com>
2020-12-19 11:03:16 +0300

    Creating router structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import Main from './src/Main';
 import {name as appName} from './app.json';
--- src/Main.js
+++ src/Main.js
@@ -1,185 +1,26 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, {useEffect, useState} from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  FlatList,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
-import {ProductCard, SearchBar} from './components';
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const products = [
-  {
-    id: 0,
-    title: 'Xiaomi Mi True Wireless Earbuds',
-    imgURL:
-      'https://m.media-amazon.com/images/I/51uguxa9nYL._AC._SR360,460.jpg',
-    price: '₺134,77',
-    inStock: true,
-  },
-  {
-    id: 1,
-    title: 'General Mobile GM 20',
-    imgURL:
-      'https://m.media-amazon.com/images/I/51lK00mvFaL._AC._SR180,230.jpg',
-    price: '₺1.810,21',
-    inStock: true,
-  },
-  {
-    id: 2,
-    title: 'Philips 58PUS8505/62 The One',
-    imgURL:
-      'https://m.media-amazon.com/images/I/71zLCzJcXaL._AC._SR360,460.jpg',
-    price: '₺6.992,25',
-    inStock: false,
-  },
-  {
-    id: 3,
-    title: 'LG 49UM7100PLB Ultra HD 4K',
-    imgURL:
-      'https://m.media-amazon.com/images/I/71gAldY8eGL._AC._SR360,460.jpg',
-    price: '₺4.614,38',
-    inStock: true,
-  },
-  {
-    id: 4,
-    title: 'Samsung Galaxy M31 SM-M315F',
-    imgURL:
-      'https://m.media-amazon.com/images/I/71mUIp9oCXL._AC._SR360,460.jpg',
-    price: '₺2.995,80',
-    inStock: true,
-  },
-  {
-    id: 5,
-    title: 'Apple AirPods Series 2',
-    imgURL:
-      'https://m.media-amazon.com/images/I/51XanmiXw0L._AC._SR360,460.jpg',
-    price: '₺1.299,00',
-    inStock: true,
-  },
-  {
-    id: 6,
-    title: 'Lenovo Tab M10 Plus',
-    imgURL:
-      'https://m.media-amazon.com/images/I/81JR-A35D0L._AC._SR360,460.jpg',
-    price: '₺2.496,50',
-    inStock: false,
-  },
-  {
-    id: 7,
-    title: 'Xiaomi Redmi 20000 Mah',
-    imgURL:
-      'https://images-na.ssl-images-amazon.com/images/I/41vVdTukkgL._AC_SX522_.jpg',
-    price: '₺134,70',
-    inStock: false,
-  },
-  {
-    id: 8,
-    title: 'Xiaomi Mijia Smart Home 360',
-    imgURL:
-      'https://images-na.ssl-images-amazon.com/images/I/31G-rIrW9zL._AC_UL320_SR226,320_.jpg',
-    price: '₺269,73',
-    inStock: true,
-  },
-  {
-    id: 9,
-    title: 'Xiaomi Mi Box S 4K Ultra HD',
-    imgURL:
-      'https://images-na.ssl-images-amazon.com/images/I/31sNKUGwNUL._AC_.jpg',
-    price: '₺478,53',
-    inStock: true,
-  },
-  {
-    id: 10,
-    title: 'Haylou Solar LS-5 Smartwatch',
-    imgURL:
-      'https://images-na.ssl-images-amazon.com/images/I/51kfZ4W9YSL._AC_SX522_.jpg',
-    price: '₺296,00',
-    inStock: true,
-  },
-];
-
-function Main() {
-  const [cartList, setCartList] = useState([]);
-  const [productList, setProductList] = useState([]);
-
-  const renderProduct = ({item}) => (
-    <ProductCard product={item} onCart={addToCart} />
-  );
-
-  function searchProduct(text) {
-    const filteredList = products.filter((product) => {
-      const itemName = product.title.toUpperCase();
-      const searchWord = text.toUpperCase();
-
-      return itemName.indexOf(searchWord) > -1;
-    });
-
-    setProductList(filteredList);
-  }
-
-  useEffect(() => {
-    setProductList(products);
-  }, []);
-
-  function addToCart(item) {
-    setCartList([...cartList, item]); // SPREAD OPERATOR
-  }
-
+function HomeScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <FlatList
-          ListEmptyComponent={
-            <Text style={styles.emptyText}>Ürün Bulunamadı..</Text>
-          }
-          ListHeaderComponent={<SearchBar onSearch={searchProduct} />}
-          keyExtractor={(item, index) => item.id.toString()}
-          data={productList}
-          renderItem={renderProduct}
-          numColumns={2}
-        />
-
-        <TouchableOpacity style={styles.cartButtonContainer}>
-          <Image
-            source={require('./assets/cart.png')}
-            style={styles.cartIcon}
-          />
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eceff1',
-  },
-  emptyText: {
-    alignSelf: 'center',
-    color: 'gray',
-    fontSize: 30,
-  },
-  cartIcon: {
-    width: 45,
-    height: 45,
-    tintColor: 'white',
-  },
-  cartButtonContainer: {
-    position: 'absolute',
-    width: 70,
-    height: 70,
-    borderRadius: 70,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'center',
-    bottom: 20,
-  },
-});
+const Stack = createStackNavigator();
 
-export default Main;
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
