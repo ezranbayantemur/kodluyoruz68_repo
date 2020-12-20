f4b0b5c010a73fc15ed7e99d0cd4dfabd8c270cf
ezranbayantemur <ezranbayantemur@gmail.com>
2020-12-20 14:59:11 +0300

    Updated news list

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,21 @@ import React from 'react';
 import {View, TouchabeOpacity, Text, Image} from 'react-native';
 import {card_item_style} from '../styles/components_style';
 
+// {
+//   "id": 0,
+//   "author": "Omkar Godbole",
+//   "title": "Bulls Exit BitMEX Bitcoin Futures Market",
+//   "description": "Since the announcement from U.S. regulators, BitMEX has witnessed an outflow of more than 40,000 bitcoins, currently worth more than $422 million.",
+//   "imageUrl": "https://static.coindesk.com/wp-content/uploads/2020/10/BitMEX-1180x628.png"
+// },
+
 const Card = ({news}) => {
   return (
     <View style={card_item_style.container}>
+      <Image style={card_item_style.image} source={{uri: news.imageUrl}} />
+      <Text style={card_item_style.title}>{news.title}</Text>
       <Text>{news.description}</Text>
+      <Text style={card_item_style.author}>{news.author}</Text>
     </View>
   );
 };
--- src/styles/components_style.js
+++ src/styles/components_style.js
@@ -1,12 +1,29 @@
 import {StyleSheet, Dimensions} from 'react-native';
 import {NewsDetail} from '../pages/NewsDetail';
 
+const deviceSize = Dimensions.get('window');
+
 const card_item_style = StyleSheet.create({
   container: {
     backgroundColor: 'white',
     borderRadius: 10,
     padding: 5,
     margin: 10,
+  },
+  image: {
+    height: deviceSize.height / 3,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 17,
+    marginTop: 5,
+  },
+  author: {
+    textAlign: 'right',
+    fontStyle: 'italic',
+    fontSize: 12,
   },
 });
 
