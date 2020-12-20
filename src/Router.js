68de93032e6681da69b00e172703138b19e6f3ee
ezranbayantemur <ezranbayantemur@gmail.com>
2020-12-20 13:12:06 +0300

    New example (NEWS)

---
+++ src/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function App() {
+  <NavigationContainer>
+      <Stack.Navigator>
+          {/* <Stack.Screen.. */}
+      </Stack.Navigator>
+  </NavigationContainer>;
+}
+
+export default App;
