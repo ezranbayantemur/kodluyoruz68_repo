532a621b84eda2dbcaaf4555d253fb4693ac7ee3
ezranbayantemur <ezranbayantemur@gmail.com>
2020-12-20 13:22:27 +0300

    Connected to route

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,19 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import {NewsDetail, NewsList} from './pages';
+
 const Stack = createStackNavigator();
 
 function App() {
-  <NavigationContainer>
-    <Stack.Navigator>{/* <Stack.Screen.. */}</Stack.Navigator>
-  </NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="List" component={NewsList} />
+        <Stack.Screen name="Detail" component={NewsDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
