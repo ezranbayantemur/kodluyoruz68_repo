446c02006cc84466113c81feec86bf32d42161b6
ezranbayantemur <ezranbayantemur@gmail.com>
2020-12-27 11:21:44 +0300

    Search bar logic

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, TextInput, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {search_bar} from '../styles/components_styles';
 
 function SearchBar(props) {
+  const [searchedValue, setSearchedValue] = useState('');
+
   return (
     <View style={search_bar.container}>
       <View style={search_bar.inputContainer}>
-        <TextInput />
+        <TextInput onChangeText={(val) => setSearchedValue(val)} />
       </View>
-      <TouchableOpacity style={search_bar.iconContainer}>
+      <TouchableOpacity
+        style={search_bar.iconContainer}
+        onPress={() => props.onSearch(searchedValue)}>
         <Icon name="magnify" color="white" size={25} />
       </TouchableOpacity>
     </View>
--- src/pages/JobList.js
+++ src/pages/JobList.js
@@ -3,10 +3,14 @@ import {SafeAreaView, View, Text} from 'react-native';
 import {SearchBar} from '../components/SearchBar';
 
 function JobList(props) {
+  function searchJob(searchedField) {
+    console.log(searchedField);
+  }
+
   return (
     <SafeAreaView>
       <View>
-        <SearchBar />
+        <SearchBar onSearch={searchJob} />
       </View>
     </SafeAreaView>
   );
