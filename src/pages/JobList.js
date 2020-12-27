8db080f760c9a181dd7330a835242c59914b5354
ezranbayantemur <ezranbayantemur@gmail.com>
2020-12-27 11:41:03 +0300

    Jobs fetched

--- src/pages/JobList.js
+++ src/pages/JobList.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import axios from 'axios';
+import React, {useState} from 'react';
+import {SafeAreaView, View, Text, FlatList} from 'react-native';
 import {SearchBar} from '../components/SearchBar';
 
+const api_url = 'https://jobs.github.com/positions.json';
+
 function JobList(props) {
+  const [jobList, setJobList] = useState([]);
+
   function searchJob(searchedField) {
-    console.log(searchedField);
+    axios
+      .get('https://jobs.github.com/positions.json?description=python')
+      .then((response) => setJobList(response.data));
   }
+
+  const renderJob = ({item}) => <Text>{item.title}</Text>;
 
   return (
     <SafeAreaView>
       <View>
         <SearchBar onSearch={searchJob} />
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={jobList}
+          renderItem={renderJob}
+        />
       </View>
     </SafeAreaView>
   );
