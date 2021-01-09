1755f6543433a7df8ea8a93148c144c6ab0e0ede
ezranbayantemur <ezranbayantemur@gmail.com>
2021-01-09 12:38:25 +0300

    Created navigation

---
+++ src/router.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function Router() {
+  <NavigationContainer></NavigationContainer>;
+}
+
+export default Router;
