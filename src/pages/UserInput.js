682c4cb64870ec6a31e4d3465559877956f70c4a
ezranbayantemur <ezranbayantemur@gmail.com>
2021-01-09 13:11:54 +0300

    Completed input

--- src/pages/UserInput.js
+++ src/pages/UserInput.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, View, Text, TextInput} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {user_input} from './styles';
 
 export function UserInput() {
+  const [mail, setMail] = useState('');
+  const [name, setName] = useState('');
+  const [surName, setSurname] = useState('');
+  const [age, setAge] = useState('');
+
+  function handleSubmit() {
+    const user = {
+      id: Math.random(),
+      mail,
+      name,
+      surName,
+      age,
+    };
+
+    console.log(user);
+  }
+
   return (
     <SafeAreaView style={user_input.container}>
       <Text style={user_input.title}>User Input</Text>
 
       <View style={user_input.inputContainer}>
-        <TextInput placeholder="E-mail address.." />
+        <TextInput
+          placeholder="E-mail address.."
+          onChangeText={(v) => setMail(v)}
+        />
       </View>
 
       <View style={user_input.inputContainer}>
-        <TextInput placeholder="Name.." />
+        <TextInput placeholder="Name.." onChangeText={(v) => setName(v)} />
       </View>
 
       <View style={user_input.inputContainer}>
-        <TextInput placeholder="Surname.." />
+        <TextInput
+          placeholder="Surname.."
+          onChangeText={(v) => setSurname(v)}
+        />
       </View>
 
       <View style={user_input.inputContainer}>
-        <TextInput placeholder="Age.." />
+        <TextInput placeholder="Age.." onChangeText={(v) => setAge(v)} />
       </View>
 
-      <TouchableOpacity style={user_input.buttonContainer}>
+      <TouchableOpacity
+        style={user_input.buttonContainer}
+        onPress={handleSubmit}>
         <Text style={user_input.buttonText}>Submit</Text>
       </TouchableOpacity>
     </SafeAreaView>
