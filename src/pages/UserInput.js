04782cdeea45fac88e188c55d28ad5dd8ddaffdd
ezranbayantemur <ezranbayantemur@gmail.com>
2021-01-09 12:55:49 +0300

    Created form inputs

--- src/pages/UserInput.js
+++ src/pages/UserInput.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, TextInput} from 'react-native';
+import {user_list} from './styles';
 
 export function UserInput() {
   return (
-    <SafeAreaView>
-      <Text>UserInput</Text>
+    <SafeAreaView style={user_list.container}>
+      <Text style={user_list.title}>User Input</Text>
+
+      <View style={user_list.inputContainer}>
+        <TextInput placeholder="E-mail address.." />
+      </View>
+
+      <View style={user_list.inputContainer}>
+        <TextInput placeholder="Name.." />
+      </View>
+
+      <View style={user_list.inputContainer}>
+        <TextInput placeholder="Surname.." />
+      </View>
+
+      <View style={user_list.inputContainer}>
+        <TextInput placeholder="Age.." />
+      </View>
     </SafeAreaView>
   );
 }
---
+++ src/pages/styles.js
@@ -0,0 +1,22 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+const user_list = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  inputContainer: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#bdbdbd',
+    borderRadius: 5,
+    margin: 10,
+    backgroundColor: 'white',
+  },
+});
+
+export {user_list};
